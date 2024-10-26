f040db68162e46fb7a09a923e7702455f1e5672f
doommchips <dgtl.bw@gmail.com>
2024-10-27 00:43:31 +0200

    add 'speculative artefact' to site

---
+++ src/lib/components/ArtOverview.svelte
@@ -0,0 +1,24 @@
+<script>
+    // Define an array of image URLs
+    let images = [
+        'https://example.com/image1.jpg',
+        'https://example.com/image2.jpg',
+        'https://example.com/image3.jpg'
+    ];    
+</script>
+
+<div>
+    {#each images as image}
+        <img src={image} alt="Artwork" class="image">
+    {/each}
+</div>
+
+<style>
+    /* Add any custom styles for your images here */
+    .image {
+        width: 200px;
+        height: 200px;
+        object-fit: cover;
+        margin: 10px;
+    }
+</style>
