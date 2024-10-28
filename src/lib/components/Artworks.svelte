8df86941fa8c13f49162d1fcb23fce303b566bd8
doommchips <dgtl.bw@gmail.com>
2024-10-29 00:52:22 +0100

    add images and videos to portfolio

---
+++ src/lib/components/Artworks.svelte
@@ -0,0 +1,196 @@
+<script>
+    // import { gsap } from 'gsap';
+    // import { fly } from 'svelte/transition';
+    import { writable } from 'svelte/store';
+
+    let artworks = [
+        { id: 1, title: 'residual/2', year: '2023', type: 'video', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/residual-2%20interaction.webm' },
+        { id: 2, title: 'residual/2', year: '2023', type: 'video', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/residual-2%20poem.webm' },
+        // { id: 3, title: 'residual/2', year: '2024', type: 'video', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/art_residual-1-waste.webm' },
+        { id: 4, title: 'residual/1', year: '2022', type: 'video', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/art_residual-1-stairs.webm' },
+        { id: 5, title: 'residual/1', year: '2022', type: 'video', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/art_residual-1-farm.webm' },
+        { id: 6, title: 'residual/1', year: '2022', type: 'video', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/art_residual-1-waste.webm' },
+        { id: 7, title: 'void_echo_02', year: '2020', type: 'video', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/art_void-echo_02_05.webm' },
+        { id: 8, title: 'void_echo_02', year: '2022', type: 'image', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/void-echo-02_beach-01.webp' },
+        { id: 9, title: 'void_echo_02', year: '2022', type: 'image', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/void-echo-02_home-01.webp' },
+        { id: 10, title: 'void_echo_02', year: '2022', type: 'image', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/void-echo-02_home-02.webp' },
+        { id: 11, title: 'void_echo_02', year: '2022', type: 'image', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/void-echo-02_home-03.webp' },
+        { id: 12, title: 'void_echo_02', year: '2022', type: 'image', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/void-echo-02_home-04.webp' },
+        { id: 12, title: 'void_echo_02', year: '2022', type: 'image', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/void-echo-02_office-01.webp' },
+        { id: 14, title: 'set-object', year: '2019', type: 'video', visualSRC: 'https://zsbqtqognaiogrpqaoot.supabase.co/storage/v1/object/public/art-records/art_new.webm' },
+        // Add more artworks here
+    ];
+
+
+
+    let selectedArtwork = writable(null);
+
+    function openLightbox(artwork) {
+        selectedArtwork.set(artwork);
+    }
+
+    function closeLightbox() {
+        selectedArtwork.set(null);
+    }
+
+    function nextArtwork() {
+        selectedArtwork.update(current => {
+            const currentIndex = artworks.findIndex(a => a.id === current.id);
+            const nextIndex = (currentIndex + 1) % artworks.length;
+            animateFwdTransition(artworks[nextIndex]);
+            return artworks[nextIndex];
+        });
+    }
+
+    function previousArtwork() {
+        selectedArtwork.update(current => {
+            const currentIndex = artworks.findIndex(a => a.id === current.id);
+            const prevIndex = (currentIndex - 1 + artworks.length) % artworks.length;
+            animateRvsTransition(artworks[prevIndex]);
+            return artworks[prevIndex];
+        });
+    }
+
+    function animateFwdTransition(newArtwork) {
+        const lightboxContent = document.querySelector('.lightbox-content img');
+        gsap.to(lightboxContent, {
+            x: '-20%',
+            duration: 0.25,
+            onComplete: () => {
+                selectedArtwork.set(newArtwork);
+                gsap.fromTo(lightboxContent, { x: '20%' }, { x: '0%', duration: 0.25 });
+            }
+        });
+    }
+
+    function animateRvsTransition(newArtwork) {
+        const lightboxContent = document.querySelector('.lightbox-content img');
+        gsap.to(lightboxContent, {
+            x: '20%',
+            duration: 0.25,
+            onComplete: () => {
+                selectedArtwork.set(newArtwork);
+                gsap.fromTo(lightboxContent, { x: '-20%' }, { x: '0%', duration: 0.25 });
+            }
+        });
+    }
+
+    // function onKeydown(e) {
+    //     switch(e.keyCode) {
+    //         case 27: // escape
+    //             closeLightbox();
+    //             break;
+    //         case 37: // left
+    //             previousArtwork();
+    //             break;
+    //         case 39: // right  
+    //             nextArtwork();
+    //             break;
+    //     }
+    // }
+</script>
+
+<div class="grid">
+    {#each artworks as artwork (artwork.id)}
+        <!-- <div class="tile" on:click={() => openLightbox(artwork)} in:fly={{ x: 200, duration: 500 }}> -->
+        <div class="tile">
+            {#if artwork.type === 'video'}
+                <video autoplay='true' loop='true' muted='true' controls='false'>
+                    <source src={artwork.visualSRC} type="video/webm">
+                    <!-- <track kind="captions" src="path/to/captions.vtt" srclang="en" label="English"> -->
+                </video>
+            {:else if artwork.type === 'image'}
+                <img src={artwork.visualSRC} alt="photo image of {artwork.title}, {artwork.year}" />
+            {/if}
+            <div class="title">
+                <p>{artwork.title}, {artwork.year}</p>
+            </div>
+        </div>
+    {/each}
+</div>
+
+{#if $selectedArtwork}
+    <div class="lightbox" on:click={closeLightbox}>
+        <div class="lightbox-content" on:click|stopPropagation>
+            <button class="nav-button prev-button" on:click={previousArtwork}>&lt;</button>
+            <img src={$selectedArtwork.visualSRC} alt={$selectedArtwork.title} />
+            <button class="nav-button next-button" on:click={nextArtwork}>&gt;</button>
+            <p>{$selectedArtwork.title}</p>
+        </div>
+    </div>
+{/if}
+
+<!-- <svelte:window on:keydown|preventDefault={onKeydown} /> -->
+
+<style>
+    .grid {
+        /* display: grid;
+        grid-template-columns: repeat(auto-fill, 400px);
+        gap: 16px; */
+        margin: 0 auto 80px auto;
+        max-width: 90vw;
+    }
+
+    .tile {
+        /* width: 400px;
+        height: 400px;
+        cursor: pointer; */
+        position: relative;
+        margin: 80px auto;
+    }
+
+    .tile img,
+    .tile video {
+        max-width: 70vw;
+        max-height: 60vh;
+        margin: 0 auto;
+        /* object-fit: cover; */
+    }
+
+    .title {
+        font-size: 16px;
+        padding: 24px;
+        text-align: center;
+    }
+
+    .lightbox {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.8);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: transform 0.5s ease;
+    }
+
+    .lightbox img {
+        max-width: 90%;
+        max-height: 90%;
+    }
+
+    .lightbox-content {
+        position: relative;
+    }
+
+    .nav-button {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        background: rgba(255, 255, 255, 0.5);
+        border: none;
+        padding: 10px;
+        cursor: pointer;
+        font-size: 18px;
+    }
+
+    .prev-button {
+        left: 10px;
+    }
+
+    .next-button {
+        right: 10px;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
     import Nav from '$lib/components/Nav.svelte';
     import Intro from '$lib/components/Intro.svelte';
+    import Artworks from '$lib/components/Artworks.svelte';
 
     // export let data;
 </script>
@@ -12,6 +13,7 @@
     <section>
         <!-- <Nav art_records={data.art_records} /> -->
         <div class="detail"><slot /></div>
+        <Artworks />
     </section>
     <footer>
         <p>upcoming: group exhibition with <a href="https://www.mondag.org/" target="_blank">M O N D A G</a> at ateliers het hoofdkantoor in haarlem. 2, 3, 9, and 10 nov 2024 [<a href="https://www.kunstlijnhaarlem.nl/expositie/order/" target="_blank">link</a>]</p>
@@ -30,8 +32,19 @@
     }
     section{
 		display: flex;
+        flex-direction: column;
         flex-grow: 10;
         gap: 24px;
 		position: relative;
+    }
+    footer{
+        background-color: var(--color-black);
+        border-top: 1px solid var(--color-white);
+        display: block;
+        padding: 24px;
+        margin: 0 -24px;
+        width: 100vw;
+        position: fixed;
+        bottom: 0;
     }
 </style>
