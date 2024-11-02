2109c2f17cfa3f722d92097373e4d9bd30393778
doommchips <dgtl.bw@gmail.com>
2024-11-02 11:37:06 +0100

    add link to home page

--- src/routes/hoofdkantoor2024/+page.svelte
+++ src/routes/hoofdkantoor2024/+page.svelte
@@ -5,12 +5,13 @@
 <p class="desc">over two years, i have been imagining a speculative city, continuously building and unbuilding it using unreal engine and blender. residual archives this through a series of interactive works, each capturing different spaces and moments from within the morphing city.</p>
 <p class="desc">this third iteration maps a virtual dwelling onto the physical boundaries of the exhibition space. moving through these coordinates, visitors encounter shadows of those who came before—my anxieties etched into their architecture, their absence more tangible than presence.</p>
 <p class="desc">this temporary inhabitation invites you to become both witness and participant in an unfolding archive of possible futures, where the boundaries between present and future, inhabited and abandoned, physical and digital, collapse.</p>
+<p class="desc"><a href="/">home</a></p>
 
 <style>
     img {
         max-width: 70vw;
         max-height: 60vh;
-        margin: 0 auto;
+        margin: 0 auto 40px auto;
     }
     .desc {
         width: 576px;
